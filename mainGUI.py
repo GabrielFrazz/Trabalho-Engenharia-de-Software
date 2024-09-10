d096da6f8a6aefc834a8639122f12d349b229546
Luan-LK <luanm_ferreira@hotmail.com>
2024-09-10 00:27:42 -0300

    Adicionando um prototipo de base de arquivos para organização do trabalho

---
+++ mainGUI.py
@@ -0,0 +1,11 @@
+#Arquivo para Importar tela principal do programa, principalmente sendo a tela inicial de login para o cliente
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+import sys
+
+window = Tk()
+
+window.geometry("400x400")
+window.configure(bg ="#FFFFFF")
+
+window.mainloop()
+#placeholder de tela para ser substituida
